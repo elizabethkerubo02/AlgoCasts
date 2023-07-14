30add3f099f73ea8a049b3c8698987cb532f9b49
keruboelizabeth02 <keruboelizabeth02@gmail.com>
2023-07-14 21:21:22 +0300

    solved palidrom using every()

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -13,4 +13,12 @@ function palindrome(str) {
     return str === reversed
 }
 
+// using every()
+function palindrome(str){
+    return str.split('').every((char, index)=>{
+        return char === str[str.length -index -1]
+
+    })
+}
+
 module.exports = palindrome;
