8d7a2d1fb4a9a37ecc2e42e674f91cf2d7be0dd8
keruboelizabeth02 <keruboelizabeth02@gmail.com>
2023-07-17 19:35:36 +0300

    maximum no. of characters appering in a string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -4,7 +4,29 @@
 // --- Examples
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
-
-function maxChar(str) {}
+function maxChar(str) {
+  
+ const charMap = {};
+  let max = 0;
+  let maxChar = '';
+    
+    for(let char of str){
+        if(!charMap[char]){
+            charMap[char] = 1;
+        }else{
+            charMap[char] ++;
+        }
+    }
+  console.log(charMap)
+  for (let char in charMap){
+    if(charMap[char] > max){
+      max = charMap[char]
+      maxChar = char
+      
+    }
+  }
+  return maxChar
+}
+console.log(maxChar('Elizabeth Kerubo Ongau Lizzie Victra'))
 
 module.exports = maxChar;
