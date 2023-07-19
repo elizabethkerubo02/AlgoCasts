fd5dc18c0785a0832705570a99943e7509110243
keruboelizabeth02 <keruboelizabeth02@gmail.com>
2023-07-19 21:18:13 +0300

    created a pyramid using #

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,22 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+    const midpoint = Math.floor((2*n - 1)/2)
+    for(row =0;row < n; row++){
+        let levels = ''
+        for(col =0; col < 2 *n -1; col++){
+            if(midpoint -row <= col && midpoint + row >= col){
+                levels += '#'
+            }else{
+                levels += ' '
+            }
+
+        }
+        console.log(levels)
+
+
+    }
+}
 
 module.exports = pyramid;
